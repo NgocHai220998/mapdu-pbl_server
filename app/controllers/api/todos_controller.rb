78d623cdbe8bafc473f9611721824b6aa4d502f9
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2021-11-16 17:26:23 +0700

    create a new todo API

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,56 +1,58 @@
 # frozen_string_literal: true
+class Api::TodosController < ApplicationController
+  before_action :authenticate_request!
+  before_action :set_work_space, only: [:create]
+  before_action :set_todo, only: [:show, :update, :destroy]
+  # GET /todos
+  def index
+    @todos = Todo.all
 
-module Api
-  class TodosController < ApplicationController
-    before_action :set_todo, only: %i[show update destroy]
+    render json: @todos
+  end
 
-    # GET /todos
-    def index
-      @todos = Todo.all
+  # GET /todos/1
+  def show
+    render json: @todo
+  end
 
-      render json: @todos
+
+  # POST /todos
+  def create
+    todo = @work_space.todos.new(todo_params)
+
+    if todo.save
+      render json: format_response(ActiveModelSerializers::SerializableResource.new(todo, {})), status: :created
+    else
+      render json: format_response_error(todo.errors.messages), status: :ok
     end
+  end
 
-    # GET /todos/1
-    def show
+  # PATCH/PUT /todos/1
+  def update
+    if @todo.update(todo_params)
       render json: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
     end
+  end
 
-    # POST /todos
-    def create
-      @todo = Todo.new(todo_params)
+  # DELETE /todos/1
+  def destroy
+    @todo.destroy
+  end
 
-      if @todo.save
-        render json: @todo, status: :created, location: @todo
-      else
-        render json: @todo.errors, status: :unprocessable_entity
-      end
-    end
+  private
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 
-    # PATCH/PUT /todos/1
-    def update
-      if @todo.update(todo_params)
-        render json: @todo
-      else
-        render json: @todo.errors, status: :unprocessable_entity
-      end
-    end
+  def set_work_space
+    @work_space = @current_user.work_spaces.find_by(id: params[:work_space_id])
 
-    # DELETE /todos/1
-    def destroy
-      @todo.destroy
-    end
+    render json: format_response_error(message: Settings.errors.work_space.work_space_not_found), status: :ok if @work_space.nil?
+  end
 
-    private
-
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo
-      @todo = Todo.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def todo_params
-      params.require(:todo).permit(:work_space_id, :title, :description, :priority, :status)
-    end
+  def todo_params
+    params.require(:todo).permit Todo::TODO_PARAMS
   end
 end
--- app/serializers/todo_serializer.rb
+++ app/serializers/todo_serializer.rb
@@ -2,4 +2,6 @@
 
 class TodoSerializer < ActiveModel::Serializer
   attributes :id, :work_space_id, :title, :description, :priority, :status
+
+  belongs_to :work_space
 end
